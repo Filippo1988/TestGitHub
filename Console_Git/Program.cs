82f597f40443a8c889b68a0111efead469f3888d
Filippo Ragni <FilippoRagni@hotmail.it>
2021-09-14 17:44:15 +0200

    Aggiunta traduzione italiano

--- Console_Git/Program.cs
+++ Console_Git/Program.cs
@@ -7,6 +7,8 @@ namespace Console_Git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello - Ciao");
         }
     }
 }
