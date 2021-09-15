78f87bde42df3a4beab2cccf1914ca8d48f75d2c
Filippo Ragni <FilippoRagni@hotmail.it>
2021-09-15 16:05:42 +0200

    creazione metodo e programma principale modificato

--- Console_Git/Program.cs
+++ Console_Git/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Gestione;
+using System.Collections.Generic;
 
 namespace Console_Git
 {
@@ -7,7 +8,19 @@ namespace Console_Git
     {
         static void Main(string[] args)
         {
-            
+            List<int> numeri;
+            numeri = new List<int>();
+            numeri.Add(1);
+            numeri.Add(2);
+            numeri.Add(3);
+            numeri.Add(4);
+            numeri.Add(5);
+
+            int somma = 0;
+            somma = Operazioni.Somma(numeri);
+            Console.WriteLine(somma);
+            Console.ReadLine();
+
 
         }
     }
--- Gestione/Operazioni.cs
+++ Gestione/Operazioni.cs
@@ -1,8 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Gestione
 {
     public class Operazioni
     {
+
+        public static int Somma(List<int> numeri)
+        {
+            int somma = 0;
+            for (int i = 0; i < numeri.Count; i++)
+            {
+                if (numeri[i] % 2 == 0)
+                    //somma = somma + numeri[i];
+                    somma = somma + numeri[1];
+            }
+            return somma;
+        }
     }
 }
